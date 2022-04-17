66f191410fdba948197b2d8fab86526a427074e2
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2022-04-17 18:35:44 -0300

    03.02 - Criação da navegação em pilha

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
-import Cesta from './src/telas/Cesta';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import AppRotas from './src/rotas/AppRotas';
 
 export default function App() {
-  const produtores = useProdutores(false);
-
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+    <SafeAreaView style={styleApp.container}>
       <StatusBar />
-      {/*<Home melhoresProdutores={false} />*/}
-      {produtores.length > 0 && (
-        <Cesta
-          produtor={{
-            nome: produtores[0].nome,
-            imagem: produtores[0].imagem,
-          }}
-          {...produtores[0].cestas[0]}
-        />
-      )}
+      <AppRotas />
     </SafeAreaView>
   );
 }
+
+const styleApp = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
